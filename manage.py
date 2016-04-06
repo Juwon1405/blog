fc0278907ceb7cee0c9add74305df78126659c4f
Juwon1405 <hotyougo@naver.com>
2016-04-06 20:16:38 +0900

    My Django Girls, 마지막

--- manage.py
+++ manage.py
@@ -2,9 +2,12 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo.settings')
+
+    # Add parent folder to path so that we can import Photologue itself.
+    PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
+    sys.path.append(os.path.join(PROJECT_PATH, ".."))
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
---
+++ start.sh
@@ -0,0 +1,2 @@
+ nohup python manage.py runserver 0:8000&
+
